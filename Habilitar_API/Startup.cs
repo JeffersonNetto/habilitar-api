cf61b412d2a739485b1765c6f603b397cf2faaa3
Jefferson Neto <jeffersonneto@live.com>
2021-03-24 22:05:18 -0300

    Adicionar arquivos de projeto.

---
+++ Habilitar_API/Startup.cs
@@ -0,0 +1,99 @@
+using FluentValidation.AspNetCore;
+using Habilitar_API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Habilitar_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            var key = Encoding.ASCII.GetBytes(Configuration["Secret"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = System.TimeSpan.Zero
+                };
+            });
+
+            services.AddControllers().AddNewtonsoftJson(_ =>
+            {
+                _.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                _.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                _.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            })
+            .AddFluentValidation();
+
+            services.AddDbContext<HabilitarContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Conexao")), ServiceLifetime.Scoped);
+
+            services
+                .AddFluentEmail(Configuration["emailAddress"] ?? "erameu.brecho@outlook.com.br")
+                .AddRazorRenderer()
+                .AddSmtpSender(new System.Net.Mail.SmtpClient("smtp.live.com", 587)
+                {
+                    Credentials = new System.Net.NetworkCredential(Configuration["emailAddress"] ?? "erameu.brecho@outlook.com.br", Configuration["emailPassword"] ?? "Tfa159357*"),
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network
+                });
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Habilitar_API", Version = "v1" });
+            });                        
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Habilitar_API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
