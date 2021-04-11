5e9be79e9f4f7ed64f280fa52f7f3e235f580ace
Jefferson Neto <jefferson@yardim.com.br>
2021-04-10 23:53:04 -0300

    ...

--- Habilitar_Frontend/src/services/LoginService.tsx
+++ Habilitar_Frontend/src/services/LoginService.tsx
@@ -1,18 +1,20 @@
 import Usuario from '../models/Usuario';
-import axios from 'axios'
+import axios from '../../src/services/interceptor'
 
-const url = 'https://localhost:5001/api/usuario/login'
+const url = 'usuario/login'
 
 export const EfetuarLogin = async (usuario: Usuario) => {
 
-    await axios.post(url, JSON.stringify(usuario), {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-    })
-    .then(res =>  console.log(res.data))
-    .catch(err => console.log(err))
+    try {
+        const response = await axios.post(url, JSON.stringify(usuario), {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })        
+    } catch (error) {
+        console.log(error)
+    }
     
 }
 
---
+++ Habilitar_Frontend/src/services/interceptor.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: 'https://localhost:5001/api/',
+    headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }    
+})
+
+axiosInstance.interceptors.request.use(async (config) => {
+    
+    if (
+        !config.url.endsWith('login') ||
+        !config.url.endsWith('refresh') ||
+        !config.url.endsWith('signup')
+    ) {
+        //const userTokenExpiration = new Date(await AsyncStorage.getItem('userTokenExpiration'));
+        //const today = new Date();
+        //if (today > userTokenExpiration) {
+        // refresh the token here
+        //   const userRefreshToken = await AsyncStorage.getItem('userRefreshToken');
+        //} else {
+        //  const userToken = await AsyncStorage.getItem('userToken');
+        //  config.headers.Authorization = `Bearer ${userToken}`;
+        //}
+
+        config.headers.Authorization = `Bearer tokenaqui`;
+
+    }    
+
+    return config;
+}, (error) => {
+    // I cand handle a request with errors here
+    return Promise.reject(error);
+});
+
+export default axiosInstance;
